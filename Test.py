afc481c3d6c32d9fd6b3ccae2fc4ff1204de350a
3048903923@qq.com <ytt123456>
2019-01-30 18:01:26 +0800

    add test

--- Test.py
+++ Test.py
@@ -1,39 +1,18 @@
-# import smtplib
-# import email.mime.multipart
-# import email.mime.text
-# from email.mime.text import MIMEText
-# from email.mime.multipart import MIMEMultipart
-# from email.mime.application import MIMEApplication
-#
-# def send_email(smtpHost,port, sendAddr, password, recipientAddrs, subject='', content=''):
-#     msg = email.mime.multipart.MIMEMultipart()
-#     msg['from'] = sendAddr
-#     msg['to'] = recipientAddrs#多个收件人的邮箱应该放在字符串中,用字符分隔, 然后用split()分开,不能放在列表中, 因为要使用encode属性
-#     msg['subject'] = subject
-#     content = content
-#     txt = email.mime.text.MIMEText(content, 'plain', 'utf-8')
-#     msg.attach(txt)
-#     print("准备添加附件...")
-#     # 添加附件，从本地路径读取。如果添加多个附件，可以定义part_2,part_3等，然后使用part_2.add_header()和msg.attach(part_2)即可。
-#     part = MIMEApplication(open('‪D:\\result\\result.html','rb').read())
-#     part.add_header('Content-Disposition', 'attachment', filename="result.html")#给附件重命名,一般和原文件名一样,改错了可能无法打开.
-#     msg.attach(part)
-#     smtp = smtplib.SMTP_SSL(smtpHost, port)#需要一个安全的连接，用SSL的方式去登录得用SMTP_SSL，之前用的是SMTP（）.端口号465或587
-#     smtp.login(sendAddr, password)#发送方的邮箱，和授权码（不是邮箱登录密码）
-#     smtp.sendmail(sendAddr, recipientAddrs.split(";"), str(msg))#注意, 这里的收件方可以是多个邮箱,用";"分开, 也可以用其他符号
-#     print("发送成功！")
-#     smtp.quit()
-#
-# if __name__ == "__main__":
-#     # try:
-#         #设置好邮箱信息
-#         smtpHost = 'smtp.qq.com'#这是QQ邮箱服务器。如果是腾讯企业邮箱，其服务器为smtp.exmail.qq.com。其他邮箱需要查询服务器地址和端口号。
-#         port = 465 #端口号
-#         sendAddr ='3048903923@qq.com'#发送方地址
-#         password = input("gaqlontktiopdcgi")#手动输入授权码更安全.授权码的获取:打开qq邮箱->设置->账户->开启IMAP/SMTP服务->发送短信->授权码
-#         recipientAddrs = '2656343994@qq.com;tingtign.yang@juneyaokc.com'#接收方可以是多个账户, 用分号分开,send_email()函数中手动设置
-#         subject='python test from yangjian'#主题
-#         content='Hello world'#正文内容
-#         send_email(smtpHost, port, sendAddr, password, recipientAddrs, subject, content)#调用函数
-#     # except:
-#     #     print("Email send fail!")
+from selenium import webdriver
+import time
+driver =webdriver.Chrome()
+driver.get("http://192.168.6.51:8080/login.jsp")
+driver.find_element_by_id("_easyui_textbox_input1").send_keys("admin")
+driver.find_element_by_id("_easyui_textbox_input2").send_keys("54321")
+driver.find_element_by_xpath("/html/body/div/div/form/p[3]/input").click()
+driver.find_element_by_link_text("文章管理").click()
+driver.find_element_by_link_text("新增").click()
+driver.find_element_by_id("_easyui_textbox_input12").send_keys("文章标题111")
+driver.find_element_by_id("_easyui_textbox_input13").send_keys("文章副标题11")
+driver.find_element_by_id("_easyui_textbox_input17").send_keys("自动化测试1")
+driver.find_element_by_css_selector("#article_submit > span").click()
+driver.find_element_by_xpath("/html/body/div[12]/div[3]/a/span").click()
+time.sleep(3)
+d=driver.switch_to_alert()
+d.accept()
+
--- TestLogin.py
+++ TestLogin.py
@@ -25,23 +25,38 @@ class TestLogin(unittest.TestCase):
             driver.find_element_by_xpath("/html/body/div/div/form/p[3]/input").click()
         except:
             print("登录失败！")
+    def test_bBewEasy(self):
+        try:
+            driver =self.driver
+            driver.find_element_by_link_text("文章管理").click()
+            driver.find_element_by_link_text("新增").click()
+            driver.find_element_by_id("_easyui_textbox_input12").send_keys("文章标题1")
+            driver.find_element_by_id("_easyui_textbox_input13").send_keys("文章副标题1")
+            driver.find_element_by_id("_easyui_textbox_input17").send_keys("自动化测试")
+            time.sleep(3)
+            driver.find_element_by_class_name("view").click()
+        except:
+            print("新增失败！")
 
-   #退出登录
+    #退出登录
     def test_Quit(self):
         try:
             driver = self.driver
+            time.sleep(3)
             driver.find_element_by_class_name("iconfont icon-tuichu font-red").click()
         except:
             print("退出失败！")
 
+   #关闭浏览器
     def test_Tclose(self):
         try:
             driver = self.driver
+            time.sleep(6)
             driver.quit()
         except:
             print("关闭浏览器失败！")
 
-
+   #清理工作
     def tearDown(self):
         time.sleep(3)
         # self.driver.quit()
@@ -57,14 +72,16 @@ if __name__ == '__main__':
         pass
     suit = unittest.TestSuite()
     suit.addTest(TestLogin('test_alogin'))
+    suit.addTest(TestLogin('test_bBewEasy'))
     suit.addTest(TestLogin('test_Quit'))
     suit.addTest(TestLogin('test_Tclose'))
+
     fp = open(report_path, 'wb')
     runner = HTMLTestReportCN.HTMLTestRunner(
         stream=fp,
         title='sish的UI自动化测试报告',
         description='报告中描述部分',
-        tester='ytt'
+        tester='杨婷婷'
     )
     # 执行测试
     runner.run(suit)

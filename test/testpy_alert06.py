a802d864c74c7a4a2f4301e8b0ffe57c39c7d0e2
3048903923@qq.com <ytt123456>
2019-02-14 15:35:20 +0800

    unittest(testsuit)

---
+++ test/testpy_alert06.py
@@ -0,0 +1,98 @@
+import unittest
+from selenium import webdriver
+import time
+import HTMLTestReportCN
+import os
+
+class test_alert(unittest.TestCase):
+    @classmethod
+    def setUp(cls):
+        cls.driver = webdriver.Firefox()
+        cls.driver.implicitly_wait(30)
+        # driver.maximize_window()
+        cls.driver.get('file:///C:/Users/hongchao.yang/Desktop/index.html')
+
+    #confirm对话框---点击确认按钮
+    def test_confirm_accept(self):
+        self.driver.find_element_by_xpath('/html/body/div/input[3]').click()
+        self.alert = self.driver.switch_to_alert()
+        self.alert.accept()
+
+    # confirm对话框---点击取消按钮
+    def test_confirm_dismiss(self):
+        self.driver.find_element_by_xpath('/html/body/div/input[3]').click()
+        alert = self.driver.switch_to_alert()
+        alert.dismiss()
+
+    #输入对话框框，输入1
+    def test_prompt_accept(self):
+        self.driver.find_element_by_xpath('/html/body/div/input[1]').click()
+        alert = self.driver.switch_to_alert()
+        alert.send_keys('1')
+        alert.accept()
+        # 输入对话框框，输入1
+
+    # 输入对话框框，输入1
+    def test_prompt_accept1(self):
+        self.driver.find_element_by_xpath('/html/body/div/input[1]').click()
+        alert = self.driver.switch_to_alert()
+        alert.send_keys('2')
+        alert.accept()
+
+    # 输入对话框框，输入1,取消
+    def test_prompt_accept2(self):
+        self.driver.find_element_by_xpath('/html/body/div/input[1]').click()
+        alert = self.driver.switch_to_alert()
+        alert.send_keys('1')
+        alert.accept()
+
+    def test_prompt_dismiss(self):
+        self.driver.find_element_by_xpath('/html/body/div/input[1]').click()
+        alert = self.driver.switch_to_alert()
+        alert.send_keys('1')
+        alert.dismiss()
+
+    def test_alert_accetp(self):
+        self.driver.find_element_by_xpath('/html/body/div/input[1]').click()
+        alert = self.driver.switch_to_alert()
+        alert.accept()
+
+    def test_alert_dismiss(self):
+        self.driver.find_element_by_xpath('/html/body/div/input[1]').click()
+        alert = self.driver.switch_to_alert()
+        alert.dismiss()
+
+    @classmethod
+    def tearDown(cls):
+        cls.driver.quit()
+
+if __name__ == '__main__':
+    path = os.getcwd()
+    report_path = path + 'result.html'
+    # 如果路径不存在，创建路径
+    if not os.path.exists(path):
+        os.makedirs(path)
+    else:
+        pass
+    suit=unittest.TestSuite()
+    suit.addTest(test_alert('test_confirm_accept'))
+    suit.addTest(test_alert('test_confirm_dismiss'))
+    suit.addTest(test_alert('test_prompt_accept1'))
+    suit.addTest(test_alert('test_prompt_accept2'))
+    suit.addTest(test_alert('test_prompt_dismiss'))
+    suit.addTest(test_alert('test_alert_accetp'))
+    suit.addTest(test_alert('test_alert_dismiss'))
+
+    fp = open(report_path, 'wb')
+    runner = HTMLTestReportCN.HTMLTestRunner(
+        stream=fp,
+        title='百度自动化搜索',
+        description='报告中描述部分',
+        tester='杨婷婷'
+    )
+    # 执行测试
+    runner.run(suit)
+
+
+
+
--- test/testpy_handle05.py
+++ test/testpy_handle05.py
@@ -1,6 +1,7 @@
 import unittest
 from selenium import webdriver
 import time
+from selenium.webdriver.common.alert import Alert
 
 class Test_BaiDu(unittest.TestCase):
     @classmethod
@@ -21,7 +22,7 @@ class Test_BaiDu(unittest.TestCase):
         time.sleep(3)
         self.driver.find_element_by_link_text('立即注册').click()
         current_handle2 = self.driver.current_window_handle
-        print('current_handle2'+current_handle2)
+        print('current_handle2：'+current_handle2)
         time.sleep(3)
         all_handles=self.driver.window_handles
         print(all_handles)
@@ -31,12 +32,19 @@ class Test_BaiDu(unittest.TestCase):
                 print("now is not in current_handle")
                 self.driver.find_element_by_id('TANGRAM__PSP_3__userName').send_keys('ytt05_091')
                 self.driver.find_element_by_id('TANGRAM__PSP_3__phone').send_keys('17633710286')
+
                 self.driver.find_element_by_id('').send_keys('ytt05_09')
-                self.driver.find_element_by_id('TANGRAM__PSP_3__verifyCodeSend').click()
-                a = input('请手动输入验证码')
-                self.driver.find_element_by_id('TANGRAM__PSP_3__verifyCode').send_keys(a)
-                self.driver.find_element_by_id('TANGRAM__PSP_3__isAgree').click()
-                self.driver.find_element_by_id('TANGRAM__PSP_3__submit').click()
+                # self.driver.find_element_by_id('TANGRAM__PSP_3__verifyCodeSend').click()
+                # a = input('请手动输入验证码')
+                # self.driver.find_element_by_id('TANGRAM__PSP_3__verifyCode').send_keys(a)
+                # self.driver.find_element_by_id('TANGRAM__PSP_3__isAgree').click()
+                # self.driver.find_element_by_id('TANGRAM__PSP_3__submit').click()
+
+            alert=self.driver.switch_to_alert()
+            
+            time.sleep(6)
+            alert.accept()
+            alert=self.driver.find_element_by_id("TANGRAM__PSP_22__confirm_continue").click()
 
 
 

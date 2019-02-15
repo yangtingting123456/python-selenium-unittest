33834650df168d8eaff53ec0042a2d5792f2029d
3048903923@qq.com <ytt123456>
2019-02-15 17:46:59 +0800

    alert handle

--- test/testpy07.py
+++ test/testpy07.py
@@ -2,6 +2,8 @@ import unittest
 from selenium import webdriver
 import time
 import os
+from selenium.webdriver import ActionChains
+
 driver = webdriver.Firefox()
 driver.implicitly_wait(30)
 driver.maximize_window()
@@ -33,8 +35,18 @@ for handle in all_handles:
         time.sleep(2)
         driver.find_element_by_id('TANGRAM__PSP_22__confirm_continue').click()
         driver.find_element_by_id('TANGRAM__PSP_3__footerULoginBtn').click()
-driver.find_element_by_link_text('忘记密码？').click()
-driver.find_element_by_id('account').send_keys('17633710286')
+
+# driver.find_element_by_link_text('忘记密码？').click()
+# driver.find_element_by_id('account').send_keys('17633710286')
+# driver.find_element_by_id('TANGRAM__PSP_3__userName').send_keys('17633710286')
+driver.find_element_by_id('TANGRAM__PSP_3__password').send_keys('ytt123456.')
+driver.find_element_by_id('TANGRAM__PSP_3__submit').click()
+driver.find_element_by_id('pass-mobile-sure-btn').click()
+time.sleep(3)
+ActionChains(driver).move_to_element(driver.find_element_by_link_text('巨蟹和别的')).perform()
+driver.find_element_by_link_text('退出').click()
+
+
 time.sleep(6)
 
 
